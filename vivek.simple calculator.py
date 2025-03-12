1b34367e78d20a83dd72b4f3d53e3a6472f5ebd6
kakuturivivek <kakuturivivek143@gmail.com>
2025-03-12 14:01:16 +0530

    Add files via upload

---
+++ vivek.simple calculator.py
@@ -0,0 +1,15 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/KGRCET/AppData/Local/Programs/Python/Python312/vivek.python simple calculator.py
+give the number:
+1.ADDITION
+2.SUBTRACTION
+3.MULTIPLICATION
+4.DIVISION
+enter the option5
+enter the first number5
+enter the second number5
+invalid option,please recheck!!
+do you want to do it again:(yes/no)n0
+do you want to do it again:no
